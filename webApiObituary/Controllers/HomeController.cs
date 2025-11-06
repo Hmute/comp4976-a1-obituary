d81dbc0b0c47b0f2356266fb698c6ab841ab9165
Hmute <heraldoabreusound@gmail.com>
2025-11-06 15:13:14 -0800

    Modified so Logout redirect works to obituary Index

--- webApiObituary/Controllers/HomeController.cs
+++ webApiObituary/Controllers/HomeController.cs
@@ -15,12 +15,12 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        return RedirectToAction("Index", "Obituary");
     }
 
     public IActionResult Privacy()
     {
-        return View();
+        return RedirectToAction("Index", "Obituary");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
