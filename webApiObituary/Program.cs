75ada1fe6907570f1f5ddb86cc02211e7f71a38d
Hmute <heraldoabreusound@gmail.com>
2025-11-06 14:31:51 -0800

    Removed, home and privacy, moved home to obituary index, changed layout so no privacy

--- webApiObituary/Program.cs
+++ webApiObituary/Program.cs
@@ -50,7 +50,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Obituary}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 app.MapIdentityApi<IdentityUser>();
